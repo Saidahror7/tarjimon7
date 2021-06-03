c50259bdb7d474b0ee8568571387816a8cf10daa
Saidahror7 <83449258+Saidahror7@users.noreply.github.com>
2021-06-03 15:53:47 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ Tarqatildi: @Api_Kod kanalida
 Demo: @GoTarjimonBot
 """
 from pyrogram import filters
-API_ID = 1662626 #bu yerga my.telegram.org manzilidan olingan Api id kiritiladi
-API_HASH = "5abb410f4fdb961906bceaf12ead92a"#bu yerga my.telegram.org manzilidan olingan Api hash kiritiladi
-TOKEN =  "bot token"#@botfather dan olingan bot token
+API_ID = 1772824 #bu yerga my.telegram.org manzilidan olingan Api id kiritiladi
+API_HASH = "345b98aa9c8c0fbafa80a6848ce69a23"#bu yerga my.telegram.org manzilidan olingan Api hash kiritiladi
+TOKEN =  "1864052936:AAG902V5fwjCSQSFjNad19IoLfltw0I4SoI"#@botfather dan olingan bot token
 
 sudousers = [1204989672] #ahamiyatsiz
 sudofilter = filters.user(sudousers)
